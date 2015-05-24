4856c7a5af288f275101efd73ebb948adf368809
DAN YU <danyu1996@gmail.com>
2015-05-23 21:08:38 -0700

    This is something

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -26,33 +26,99 @@ Template.home.events({
         // display the error to the user
         console.log(error);
       } else {
-        //Router.go('postPage', {_id: currentPostId});
-        console.log("This worked");
+
+        // Show the user his current video and wait.
         Session.set('user', usernamesProperties.content);
         Router.go('/');
+
+        console.log("hello");
+
+        // Create the end point for the application.
+        endpoint = Twilio.Endpoint(accessToken);
+        endpoint.listen().then(
+          function (error) {
+            log('Could not connect to Twilio: ' + error.message);
+          }
+        );
+        if (!previewMedia) {
+          previewMedia = new Twilio.LocalMedia();
+          Twilio.getUserMedia().then(
+            function (mediaStream) {
+              previewMedia.addStream(mediaStream);
+              previewMedia.attach('#local-media');
+            },
+            function (error) {
+              console.error('Unable to access local media', error);
+              log('Unable to access Camera and Microphone');
+            }
+          );
+        };
       }
     });
   },
   'click #button-preview': function(e) {
     e.preventDefault();
-    endpoint = new Twilio.Endpoint(accessToken);
-    endpoint.listen().then(
-      function (error) {
-        log('Could not connect to Twilio: ' + error.message);
-      }
-    );
-    if (!previewMedia) {
-      previewMedia = new Twilio.LocalMedia();
-      Twilio.getUserMedia().then(
-        function (mediaStream) {
-          previewMedia.addStream(mediaStream);
-          previewMedia.attach('#local-media');
-        },
-        function (error) {
-          console.error('Unable to access local media', error);
-          log('Unable to access Camera and Microphone');
-        }
-      );
-    };
+    console.log(twilioConfig);
+
   }
 });
+
+// successfully connected!
+function endpointConnected() {
+  document.getElementById('invite-controls').style.display = 'block';
+  log("Connected to Twilio. Listening for incoming Invites as '" + endpoint.address + "'");
+
+  endpoint.on('invite', function (invite) {
+    log('Incoming invite from: ' + invite.from);
+    invite.accept().then(conversationStarted);
+  });
+
+  // bind button to create conversation
+  document.getElementById('button-invite').onclick = function () {
+    var inviteTo = document.getElementById('invite-to').value;
+
+    if (activeConversation) {
+      // add a participant
+      activeConversation.invite(inviteTo);
+    } else {
+      // create a conversation
+      var options = {};
+      if (previewMedia) {
+        options.localMedia = previewMedia;
+      }
+      endpoint.createConversation(inviteTo, options).then(
+        conversationStarted,
+        function (error) {
+          log('Unable to create conversation');
+          console.error('Unable to create conversation', error);
+        }
+      );
+    }
+  };
+};
+
+// conversation is live
+function conversationStarted(conversation) {
+  log('In an active Conversation');
+  activeConversation = conversation;
+  // draw local video, if not already previewing
+  if (!previewMedia) {
+    conversation.localMedia.attach('#local-media');
+  }
+  // when a participant joins, draw their video on screen
+  conversation.on('participantConnected', function (participant) {
+    log("Participant '" + participant.address + "' connected");
+    participant.media.attach('#remote-media');
+  });
+  // when a participant disconnects, note in log
+  conversation.on('participantDisconnected', function (participant) {
+    log("Participant '" + participant.address + "' disconnected");
+  });
+  // when the conversation ends, stop capturing local video
+  conversation.on('ended', function (conversation) {
+    log("Connected to Twilio. Listening for incoming Invites as '" + endpoint.address + "'");
+    conversation.localMedia.stop();
+    conversation.disconnect();
+    activeConversation = null;
+  });
+};
