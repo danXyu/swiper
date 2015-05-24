982a71e0bfa7de0f3a9b0bd557a0a12215f5ee83
DAN YU <danyu1996@gmail.com>
2015-05-23 20:17:45 -0700

    changes

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -6,17 +6,6 @@ var accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1zYXQ7d
 Template.home.helpers({
   'activeUser' : function () {
     return Session.get('user') != null;
-  },
-  'semanticElement' : function () {
-    return [
-      { 'what' : 'Large Buttons', 'withBootstrap' : 'btn btn-lg', 'withSemanticUI' : 'ui large button' },
-      { 'what' : 'One column', 'withBootstrap' : 'col-md-1', 'withSemanticUI' : 'one wide column' },
-      { 'what' : 'Vertical Menu / Navigation', 'withBootstrap' : 'nav nav-pills', 'withSemanticUI' : 'ui vertical menu' }
-
-    ];
-  },
-  'bootstrapCode' : function () {
-    return '<div class="btn btn-primary btn-lg"></div>';
   }
 });
 
